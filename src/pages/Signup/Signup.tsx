e3c56026b9e26460b54e4bf45553e99c43114782
hahs <alexandre_2420@outlook.com>
2022-10-28 17:20:43 -0500

    validations implementation

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,4 +1,5 @@
 import { useFormik } from "formik";
+import * as Yup from "yup";
 //styles
 import styles from "./signup.module.css";
 
@@ -9,11 +10,21 @@ export function Signup() {
       lastName: "",
       email: "",
     },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(15, "Must be 20 characters or less")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
     onSubmit: (values) => {
       console.log(values);
       formik.resetForm();
     },
   });
+  //   console.log(formik.errors);
   return (
     <form className={styles.form} onSubmit={formik.handleSubmit}>
       <section className={styles.inputContainer}>
@@ -24,7 +35,11 @@ export function Signup() {
           placeholder="FirstName"
           value={formik.values.firstName}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
+        {formik.touched.firstName && formik.errors.firstName && (
+          <p>{formik.errors.firstName}</p>
+        )}
       </section>
       <section className={styles.inputContainer}>
         <input
@@ -34,7 +49,11 @@ export function Signup() {
           placeholder="LastName"
           value={formik.values.lastName}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
+        {formik.touched.lastName && formik.errors.lastName && (
+          <p>{formik.errors.lastName}</p>
+        )}
       </section>
       <section className={styles.inputContainer}>
         <input
@@ -44,7 +63,11 @@ export function Signup() {
           placeholder="Email"
           value={formik.values.email}
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
+        {formik.touched.email && formik.errors.email && (
+          <p>{formik.errors.email}</p>
+        )}
       </section>
 
       <section>
