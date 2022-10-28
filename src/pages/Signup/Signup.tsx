5573ec2ed67b9ccf6abb30e20a7f19d6c5351202
hahs <alexandre_2420@outlook.com>
2022-10-28 15:41:27 -0500

    useFormik implementation

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,14 +1,29 @@
+import { useFormik } from "formik";
+//styles
 import styles from "./signup.module.css";
 
 export function Signup() {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+      formik.resetForm();
+    },
+  });
   return (
-    <form>
+    <form className={styles.form} onSubmit={formik.handleSubmit}>
       <section className={styles.inputContainer}>
         <input
           type="text"
           id="firstName"
           name="firstName"
           placeholder="FirstName"
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
         />
       </section>
       <section className={styles.inputContainer}>
@@ -17,10 +32,23 @@ export function Signup() {
           id="lastName"
           name="lastName"
           placeholder="LastName"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
         />
       </section>
       <section className={styles.inputContainer}>
-        <input type="email" id="email" name="email" placeholder="Email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+      </section>
+
+      <section>
+        <button type="submit">Send</button>
       </section>
     </form>
   );
